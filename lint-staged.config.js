93daaf38bb2bfa05fd3d70c6a385d4be0d9fa4e5
Fernando Blasco <fblascon@gmail.com>
2026-04-29 11:41:54 +0200

    feat: add typecheck to pre-commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
-  '*.{ts,js}': ['eslint --fix', 'prettier --write'],
+  '*.{ts,js}': ['eslint --fix', 'prettier --write', 'tsc --noEmit'],
   '*.{html,scss,css,json,md}': ['prettier --write'],
 };
